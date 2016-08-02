9977999be311112abc804b2d4a995641d61861d3
pag <prajeeshag@gmail.com>
2016-08-02 13:41:45 +0530

    Added legendre recursive function.

--- recursive_func/main.f90
+++ recursive_func/main.f90
@@ -1,11 +1,20 @@
 program main
-	use recursive_mod, only: factorial
-	implicit none
-	
-	integer :: n=0
+  use recursive_mod, only: factorial, legendre
+  implicit none
+  
+  integer :: n=0, i
+  real, dimension(-10:10) :: x, y
+
+  forall(i=-10:10) x(i)=real(i)/10.0
+
+  print *, x
 
   print *, 'enter a positive integer :'
-	read(*,*) n
+  read(*,*) n
 
-	write(*,*) factorial(n)
+  do i=-10,10
+    y(i) = legendre(n,x(i))
+  enddo
+
+  print *, y
 end program main
--- recursive_func/recursive_mod.f90
+++ recursive_func/recursive_mod.f90
@@ -1,17 +1,33 @@
 module recursive_mod
-	implicit none
-	
-	contains
+  implicit none
+  
+  contains
 
-	recursive function factorial(n) result(res)
-	  integer :: res, n
-	  if (n == 0) then
-	      res = 1
-	  elseif(n > 0) then
-	      res = n * factorial(n - 1)
-		else
-				stop 'Error: Factorial not defined for negative integers'
-	  end if
-	end
+  recursive function factorial(n) result(res)
+    integer, intent(in) :: n
+    integer :: res
+    if (n == 0) then
+        res = 1
+    elseif(n > 0) then
+        res = n * factorial(n - 1)
+    else
+        stop 'Error: Factorial not defined for negative integers'
+    end if
+  end function factorial
+
+  recursive function legendre(n,x) result(y)
+    integer, intent(in) :: n
+    real, intent(in) :: x
+    real :: y
+
+    if ( n == 0 ) then
+      y = 1.0
+    else if ( n == 1 ) then
+      y = x
+    else
+      y = ( (2*n-1) * x * legendre(n-1,x) - &
+            (n-1) * legendre(n-2,x) ) / n
+    endif
+  end function legendre
 
 end module recursive_mod

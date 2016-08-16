e3ac05c83f65102e728438186c76c649042ce602
pag <prajeeshag@gmail.com>
2016-08-16 19:24:46 +0530

    Added flooding in long_surf_gravity_waves.

--- long_surf_gravity_waves/long_surf_gravity_waves.f90
+++ long_surf_gravity_waves/long_surf_gravity_waves.f90
@@ -5,12 +5,13 @@ program main
   real, parameter :: GRAV = 9.8
   real :: delt = 0.1, delx = 10.0, dely = 10.0
   integer :: ntstep=2000
-  integer :: ni = 100, nj = 100, nip1, njp1
+  integer :: ni = 150, nj = 100, nip1, njp1
   real, allocatable :: uvel(:,:,:), vvel(:,:,:), eta(:,:,:), h(:,:), h0(:,:), topo(:,:)
   real, allocatable :: xaxis(:), yaxis(:), tmp(:,:)
-  logical, allocatable :: maskt(:,:)
+  logical, allocatable :: wet(:,:)
   integer :: i, j, t, taup1, tau 
-  real :: rdelx, rdely, cfl, temp_b1, temp_b2, temp_a1, temp_a2, epsl=0.005
+  real :: rdelx, rdely, cfl, temp_b1, temp_b2, temp_a1, temp_a2, epsl=0.0
+  real :: hmin = 0.05, tmpvel
   logical :: exist
   
   namelist/main_nml/ delt, delx, dely, ni, nj, ntstep, epsl
@@ -27,46 +28,49 @@ program main
   
   nip1 = ni + 1; njp1 = nj + 1
   rdelx = 1./delx ; rdely = 1./dely
+  temp_a1 = delt * GRAV * rdelx
+  temp_a2 = delt * GRAV * rdely 
+  temp_b1 = delt * rdelx * 0.5
+  temp_b2 = delt * rdely * 0.5
   
   
   allocate(uvel(0:1,1:nip1,1:njp1))
   allocate(vvel(0:1,1:nip1,1:njp1))
-  allocate(eta(0:1,1:ni,1:nj))
+  allocate(eta(0:1,0:nip1,0:nip1))
   allocate(h(0:nip1,0:njp1))
   allocate(h0(1:ni,1:nj))
   allocate(tmp(ni,nj))
   allocate(topo(1:ni,1:nj))
-  allocate(maskt(0:nip1,0:njp1))
+  allocate(wet(0:nip1,0:njp1))
   allocate(xaxis(nip1))
   allocate(yaxis(njp1))
 
   forall(i=1:nip1) xaxis(i) = i
   forall(i=1:njp1) yaxis(i) = i
 
-  topo(:,:) = -10.0
-  topo(1,:) = 5.0
-  topo(ni,:) = 5.0
-  topo(:,1) = 5.0
-  topo(:,nj) = 5.0
+  topo(:,:) = 10.0
+  topo(1,:) = -5.0
+  topo(ni,:) = -5.0
+  forall(i=-5:5) topo(100+i-1,:) = 2*abs(i) - 0.1
+  topo(:,1) = -5.0
+  topo(:,nj) = -5.0
 
-  maskt(:,:) = .false.
 
-  h0(:,:) = 0.0; eta(:,:,:) = 0.0; uvel(:,:,:) = 0.0; h(:,:) = 0.0; vvel = 0.0
+  uvel(:,:,:) = 0.0; vvel = 0.0
+
+  h0(:,:) = topo(:,:)
+
+  eta(:,:,:) = 0.0
   
-  where(topo(:,:) < 0.0) maskt(1:ni,1:nj)=.true.
+  where (h0<0.0) eta(0,1:ni,1:nj) = -1.0*h0(:,:)
+
+  do i = 1, 11
+     do j = 1, 11
+        eta(:,65+i,int(nj/2)-6+j) = sin(3.1414*(i-1)/11)*sin(3.1414*(j-1)/11)
+     enddo
+  enddo
   
-  eta(:,int(ni/2)-5:int(ni/2)+5,int(nj/2)-5:int(nj/2)+5) = 0.5
-  
-  where (maskt(1:ni,1:nj)) h0(:,:) = -1.0*topo(:,:)
-  
-  temp_a1 = delt * GRAV * rdelx
-  temp_a2 = delt * GRAV * rdely 
-  temp_b1 = delt * rdelx * 0.5
-  temp_b2 = delt * rdely * 0.5
-  
-  h(1:ni,1:nj) = h0(:,:) + eta(0,:,:)
-  
-  cfl = delt * sqrt(GRAV * maxval(h)) / delx
+  cfl = delt * sqrt(GRAV * maxval(h0)) / delx
 
   print *, 'cfl=',cfl
   if ( cfl >= 1.0 ) then
@@ -74,18 +78,37 @@ program main
      stop 'ERROR'
   endif
   
+  h(0,:) = hmin
+  h(nip1,:) = hmin
+  h(:,0) = hmin
+  h(:,njp1) = hmin
+
+  eta(:,0,:) = 0.0
+  eta(:,nip1,:) = 0.0
+  eta(:,:,0) = 0.0
+  eta(:,:,njp1) = 0.0
+     
+  h(1:ni,1:nj) = h0(:,:) + eta(0,1:ni,1:nj)
+  
+  wet(:,:) = .true.
+  
+  where(h(1:ni,1:nj) < hmin) wet(1:ni,1:nj)=.false.
+  
   do t = 1, ntstep
      taup1 = mod(t,2)
      tau = mod(t+1,2)
 
      do j = 1, njp1
         do i = 1, nip1
-           if (maskt(i,j).and.maskt(i-1,j)) then
-              uvel(taup1,i,j) = uvel(tau,i,j) - temp_a1 * (eta(tau,i,j)-eta(tau,i-1,j))
-           endif
-           if (maskt(i,j).and.maskt(i,j-1)) then
-              vvel(taup1,i,j) = vvel(tau,i,j) - temp_a2 * (eta(tau,i,j)-eta(tau,i,j-1))
-           endif
+           tmpvel = uvel(tau,i,j) - temp_a1 * (eta(tau,i,j)-eta(tau,i-1,j))
+           if (.not. wet(i-1,j) .and. tmpvel > 0.0) tmpvel = 0.0
+           if (.not. wet(i,j) .and. tmpvel < 0.0) tmpvel = 0.0
+           uvel(taup1,i,j) = tmpvel
+           
+           tmpvel = vvel(tau,i,j) - temp_a2 * (eta(tau,i,j)-eta(tau,i,j-1))
+           if (.not. wet(i,j-1) .and. tmpvel > 0.0) tmpvel = 0.0
+           if (.not. wet(i,j) .and. tmpvel < 0.0) tmpvel = 0.0
+           vvel(taup1,i,j) = tmpvel
         enddo
      enddo
      
@@ -102,15 +125,22 @@ program main
                                                      - (vvel(taup1,i,j) - abs(vvel(taup1,i,j))) * h(i,j) )
         enddo
      enddo
-
-     call shapiro_filter_2d(eta(taup1,:,:))
      
-     h(1:ni,1:nj) = h0(:,:) + eta(taup1,:,:)
+     h(1:ni,1:nj) = h0(:,:) + eta(taup1,1:ni,1:nj)
+      
+     wet(:,:) = .true.
+      
+     where(h(1:ni,1:nj) < hmin) wet(1:ni,1:nj)=.false.
+
+     call shapiro_filter_2d(eta(taup1,1:ni,1:nj),wet(1:ni,1:nj))
+     
      tmp = -999.0
-     where(h(1:ni,1:nj)>0.0) tmp(:,:) = h(1:ni,1:nj)
-     call write_nc(xaxis(1:ni), yaxis(1:nj), tmp, 'h', t, missing_value=-999.0)
-     call write_nc(xaxis(1:ni), yaxis(1:nj), uvel(taup1,1:ni,1:nj), 'uvel', t, missing_value=-999.0)
-     call write_nc(xaxis(1:ni), yaxis(1:nj), vvel(taup1,1:ni,1:nj), 'vvel', t, missing_value=-999.0)
+     where(wet(1:ni,1:nj)) tmp(:,:) = eta(taup1,1:ni,1:nj)
+     
+     call write_nc(xaxis(1:ni), yaxis(1:nj), tmp, 'eta', t, missing_value=-999.0)
+     call write_nc(xaxis(1:ni), yaxis(1:nj), h0, 'h0', t)
+!     call write_nc(xaxis(1:ni), yaxis(1:nj), uvel(taup1,1:ni,1:nj), 'uvel', t, missing_value=-999.0)
+!     call write_nc(xaxis(1:ni), yaxis(1:nj), vvel(taup1,1:ni,1:nj), 'vvel', t, missing_value=-999.0)
 
   enddo
 
@@ -120,12 +150,13 @@ program main
   deallocate(h)
   deallocate(h0)
   deallocate(topo)
-  deallocate(maskt)
+  deallocate(wet)
 
 contains
 
-  subroutine shapiro_filter_2d(field)
+  subroutine shapiro_filter_2d (field, mask)
     real, intent(inout) :: field(:,:)
+    logical, intent(in) :: mask(:,:)
     integer :: is, ie, js, je, i, j
     
     is = 2; ie = size(field,1) - 1
@@ -133,6 +164,10 @@ contains
 
     do i = is, ie
        do j = js, je, 1
+          if (.not.mask(i-1,j)) cycle
+          if (.not.mask(i+1,j)) cycle
+          if (.not.mask(i,j-1)) cycle
+          if (.not.mask(i,j+1)) cycle
           field(i,j) = (1-epsl)*field(i,j) + 0.25 * epsl * &
                (field(i-1,j)+field(i+1,j)+field(i,j-1)+field(i,j+1))
        end do
